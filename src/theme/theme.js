d8b5f0586f3a72357856ef302b6d96a77a8c16da
diamondrarktvo <amada.rktvo@gmail.com>
2023-07-20 11:59:47 +0300

    feat: add breakpoints

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -51,7 +51,15 @@ const theme = createTheme({
   },
   typography: {
     ...typo
-  }
+  },
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 640,
+      laptop: 1024,
+      desktop: 1200,
+    },
+  },
 });
 
 export default theme;
