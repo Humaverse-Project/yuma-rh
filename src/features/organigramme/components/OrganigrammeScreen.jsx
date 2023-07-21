0ffcd783961a77dbb9a0633e0eecf1f92a5f3427
romshaj <134253205+romshaj@users.noreply.github.com>
2023-07-21 23:00:08 +0300

    Update OrganigrammeScreen.jsx

--- src/features/organigramme/components/OrganigrammeScreen.jsx
+++ src/features/organigramme/components/OrganigrammeScreen.jsx
@@ -1,28 +1,89 @@
-import { Box } from '@mui/system'
-import { Fragment } from 'react'
-import { Text } from '../../../shared'
-import HeaderInScreen from '../../header/HeaderInScreen'
+import React from 'react';
+import HeaderInScreen from '../../header/HeaderInScreen';
+import { Box, Typography } from '@mui/material';
+import './Organigramme.css';
 
-//ICONES
+const OrganigrammeNode = ({ data }) => (
+  <Box className="node" p={2} m={1}>
+    <Typography variant="h6" color="primary">
+      {data.name}
+    </Typography>
+    <Typography variant="body1" color="textSecondary">
+      {data.position}
+    </Typography>
+    {data.children && (
+      <Box display="flex" flexDirection="row" mt={2}>
+        {data.children.map((child) => (
+          <OrganigrammeNode key={child.name} data={child} />
+        ))}
+      </Box>
+    )}
+  </Box>
+);
+
+const organigrammeData = {
+  name: 'PDG',
+  position: 'CEO',
+  children: [
+    {
+      name: 'Responsable RH',
+      position: 'Responsable RH',
+      children: [
+        {
+          name: 'Gestion du personnel',
+          position: 'Manager',
+          children: [
+            {
+              name: 'Gestion des performances',
+              position: 'Superviseur',
+              children: [
+                { name: 'Formation', position: 'Formateur' },
+                { name: 'Avantages sociaux', position: 'Responsable des avantages sociaux' },
+              ],
+            },
+          ],
+        },
+        {
+          name: 'Recrutement',
+          position: 'Manager',
+          children: [
+            {
+              name: 'Gestion des talents',
+              position: 'Chasseur de têtes',
+              children: [
+                { name: 'Sourcing', position: 'Chargé de sourcing' },
+                { name: 'Entretiens', position: 'Responsable des entretiens' },
+              ],
+            },
+            { name: 'Relations avec les candidats', position: 'Recruteur' },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
 function OrganigrammeScreen() {
-    return (
-        <Fragment>
-            <HeaderInScreen title={'Organigramme'} />
-            <Box
-                backgroundColor="background.paper"
-                display={'flex'}
-                flexDirection="column"
-                justifyContent="center"
-                alignItems="center"
-                height={'88vh'}
-            >
-                <Text variant={'h1'} color="blue.main">
-                    Organigramme
-                </Text>
-            </Box>
-        </Fragment>
-    )
+  return (
+    <React.Fragment>
+      <HeaderInScreen />
+      <Box
+        backgroundColor="background.paper"
+        display={'flex'}
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        minHeight={'88vh'}
+        p={2}
+      >
+        <Typography variant="h4" color="primary">
+          Modèle d'organigramme hiérarchique
+        </Typography>
+        <Box className="organigramme" mt={4}>
+          <OrganigrammeNode data={organigrammeData} />
+        </Box>
+      </Box>
+    </React.Fragment>
+  );
 }
-
-export default OrganigrammeScreen
+export default OrganigrammeScreen;
