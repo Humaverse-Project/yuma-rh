644128f5ee922b9b2e72a74573fc368ae2ee98ef
diamondrarktvo <amada.rktvo@gmail.com>
2023-07-22 21:33:35 +0300

    remove code inutile

--- src/features/header/HeaderInOrgan.jsx
+++ src/features/header/HeaderInOrgan.jsx
@@ -1,31 +1,32 @@
-import { DynamicHeadNav, Row } from '../../shared';
-import { Button, useMediaQuery } from '@mui/material';
+import { DynamicHeadNav, Row } from '../../shared'
+import { Button, useMediaQuery } from '@mui/material'
 
 // ICONES
-import LanguageIcon from '@mui/icons-material/Language';
-import SettingsIcon from '@mui/icons-material/Settings';
-import { useTheme } from '@emotion/react';
+import { useTheme } from '@emotion/react'
 
 function HeaderInOrgan() {
-  const theme = useTheme();
-  const matcheSM = useMediaQuery(theme.breakpoints.up('sm'));
-  const matcheMD = useMediaQuery(theme.breakpoints.down('md'));
+    const theme = useTheme()
+    const matcheSM = useMediaQuery(theme.breakpoints.up('sm'))
 
-  return (
-    <Row justifyContent={'space-between'} px={3} height={'10vh'}>
-      <DynamicHeadNav title={'Organigramme'} />
-      {matcheSM && (
-        <Row>
-          <Button variant="contained" color="primary" sx={{ marginRight: '16px' }}>
-            Créer un poste
-          </Button>
-          <Button variant="contained" color="primary">
-            Connection SIRH
-          </Button>
+    return (
+        <Row justifyContent={'space-between'} px={3} height={'10vh'}>
+            <DynamicHeadNav title={'Organigramme'} />
+            {matcheSM && (
+                <Row>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        sx={{ marginRight: '16px' }}
+                    >
+                        Créer un poste
+                    </Button>
+                    <Button variant="contained" color="primary">
+                        Connection SIRH
+                    </Button>
+                </Row>
+            )}
         </Row>
-      )}
-    </Row>
-  );
+    )
 }
 
-export default HeaderInOrgan;
+export default HeaderInOrgan
