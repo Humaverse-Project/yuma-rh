703692de356b458d304fd7fbc0b836aeca38e7d0
diamondrarktvo <amada.rktvo@gmail.com>
2023-07-22 22:44:01 +0300

    feat: add organigramme tree examples in organigramme screen

--- src/features/organigramme/components/OrganigrammeScreen.jsx
+++ src/features/organigramme/components/OrganigrammeScreen.jsx
@@ -1,96 +1,13 @@
 import { Fragment, useState } from 'react'
 import HeaderInScreen from '../../header/HeaderInScreen'
 import { useTheme } from '@mui/material/styles'
-import { Box, InputAdornment, TextField, Typography } from '@mui/material'
+import { Box, InputAdornment, TextField } from '@mui/material'
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
 import FormGroup from '@mui/material/FormGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Checkbox from '@mui/material/Checkbox'
 import './Organigramme.css'
-
-const OrganigrammeNode = ({ data }) => (
-    <Box
-        className="node"
-        p={2}
-        m={1}
-        border={1}
-        borderColor="primary.main"
-        borderRadius={4}
-    >
-        <Box>
-            <Typography variant="h6" color="primary">
-                {data.name}
-            </Typography>
-        </Box>
-        <Box>
-            <Typography variant="body1" color="textSecondary">
-                {data.position}
-            </Typography>
-        </Box>
-        {data.children && (
-            <Box display="flex" flexDirection="row" mt={2}>
-                {data.children.map((child) => (
-                    <OrganigrammeNode key={child.name} data={child} />
-                ))}
-            </Box>
-        )}
-    </Box>
-)
-
-const organigrammeData = {
-    name: 'PDG',
-    position: 'CEO',
-    children: [
-        {
-            name: 'Responsable RH',
-            position: 'Responsable RH',
-            children: [
-                {
-                    name: 'Gestion du personnel',
-                    position: 'Manager',
-                    children: [
-                        {
-                            name: 'Gestion des performances',
-                            position: 'Superviseur',
-                            children: [
-                                { name: 'Formation', position: 'Formateur' },
-                                {
-                                    name: 'Avantages sociaux',
-                                    position:
-                                        'Responsable des avantages sociaux',
-                                },
-                            ],
-                        },
-                    ],
-                },
-                {
-                    name: 'Recrutement',
-                    position: 'Manager',
-                    children: [
-                        {
-                            name: 'Gestion des talents',
-                            position: 'Chasseur de têtes',
-                            children: [
-                                {
-                                    name: 'Sourcing',
-                                    position: 'Chargé de sourcing',
-                                },
-                                {
-                                    name: 'Entretiens',
-                                    position: 'Responsable des entretiens',
-                                },
-                            ],
-                        },
-                        {
-                            name: 'Relations avec les candidats',
-                            position: 'Recruteur',
-                        },
-                    ],
-                },
-            ],
-        },
-    ],
-}
+import StyledTreeExample from './OrganigrammeTree'
 
 function OrganigrammeScreen() {
     const theme = useTheme()
@@ -112,8 +29,9 @@ function OrganigrammeScreen() {
                 }}
                 justifyContent="center"
                 alignItems="flex-start"
-                minHeight="88vh"
+                minHeight="80vh"
                 py={6}
+                px={4}
             >
                 <Box>
                     <TextField
@@ -144,8 +62,8 @@ function OrganigrammeScreen() {
                         />
                     </FormGroup>
                 </Box>
-                <Box className="organigramme">
-                    <OrganigrammeNode data={organigrammeData} />
+                <Box className="organigramme" sx={{ display: 'flex', flex: 2 }}>
+                    <StyledTreeExample />
                 </Box>
             </Box>
         </Fragment>
