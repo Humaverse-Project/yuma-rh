f1fca570400116bf6ab2988313bac5aeba3696e2
Félin Aldric <fel1aldric@gmail.com>
2023-08-11 11:23:01 +0300

    [style] debut amelioration interface

--- src/features/nomenclature/components/MetierScreen.jsx
+++ src/features/nomenclature/components/MetierScreen.jsx
@@ -25,7 +25,7 @@ function MetierScreen() {
           // Utilisation de l'access token pour l'appel API GET avec Authorization
           getFicheMetierData(data.access_token)
             .then((data) => {
-              
+              console.log(data)
               const formattedData = data.map((item) => ({
                 code: item.code,
                 libelle: item.metier.libelle,
@@ -58,81 +58,11 @@ function MetierScreen() {
         [],
     );
 
-    if (loading) {
-      return (
-        <Fragment>
-            <HeaderInScreen
-                title={'Liste metier'}
-            />
-            <Box
-                backgroundColor="background.paper"
-                display={'flex'}
-                flexDirection="column"
-                justifyContent="center"
-                alignItems="center"
-                height={'auto'}
-                minHeight="80vh"
-            >
-                <Grid container spacing={2}>
-                    <Grid item xs={12} md={9}
-                        sx={{
-                            [theme.breakpoints.up('lg')]: {
-                                mt: 5,
-                            },
-                            [theme.breakpoints.down('sm')]: {
-                                my: 1,
-                                mx: 0,
-                            },
-                        }}
-                    >
-                        <div>Loading...</div>
-                    </Grid>
-                </Grid>
-            </Box>
-        </Fragment>
-      );
-    }
-  
-    if (error) {
-        return (
-            <Fragment>
-                <HeaderInScreen
-                    title={'Liste metier'}
-                />
-                <Box
-                    backgroundColor="background.paper"
-                    display={'flex'}
-                    flexDirection="column"
-                    justifyContent="center"
-                    alignItems="center"
-                    height={'auto'}
-                    minHeight="80vh"
-                >
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} md={9}
-                            sx={{
-                                [theme.breakpoints.up('lg')]: {
-                                    mt: 5,
-                                },
-                                [theme.breakpoints.down('sm')]: {
-                                    my: 1,
-                                    mx: 0,
-                                },
-                            }}
-                        >
-                            <div>Error: {error}</div>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Fragment>
-        )
-    }
-  
     // Affichez les données récupérées
     return (
       <Fragment>
         <HeaderInScreen
-            title={'Liste metier'}
+            title={'Liste fiche métier'}
         />
         <Box
             backgroundColor="background.paper"
