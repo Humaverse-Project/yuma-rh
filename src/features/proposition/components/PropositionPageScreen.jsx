33b19e892449e315add5ba6056e18eab4590449d
Félin Aldric <fel1aldric@gmail.com>
2023-08-22 15:15:12 +0300

    [feat] affichage v1

--- src/features/proposition/components/PropositionPageScreen.jsx
+++ src/features/proposition/components/PropositionPageScreen.jsx
@@ -3,14 +3,19 @@ import React, { Fragment, useState, useEffect, useMemo } from 'react';
 import { listmetier } from  '../../../services/MetierService';
 import { listcompetance } from  '../../../services/CompetanceService';
 import { listpost } from  '../../../services/PosteService';
-import { listproposition, postPropositionPoste, postPropositionPosteNew } from  '../../../services/PropositionService';
+import { listproposition, postPropositionPoste } from  '../../../services/PropositionService';
 import { useTheme } from '@mui/material/styles'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid'
 import { LoadingMetier, TableMetier } from '../../../shared'
-import { Button, Typography } from '@mui/material';
+import { Button, Link, Typography } from '@mui/material';
 import ModalEdit from './ModalEdit';
 import ModalCreate from './ModalCreate';
+import EditIcon from '@mui/icons-material/Edit';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import BackspaceIcon from '@mui/icons-material/Backspace';
 
 function PropositionPageScreen() {
     const theme = useTheme()
@@ -33,14 +38,35 @@ function PropositionPageScreen() {
     const handleCloseCreate = () => {
         setOpencreate(false)
     }
+    const mapingdata = (datatomap) =>{
+        const datatoformat = datatomap.map(data=>{
+            return {
+                id: data.id,
+                metier: data.propositionPostes[0].metier.code+" "+data.propositionPostes[0].metier.nom,
+                like: 0,
+                unlike: 0,
+                creation: data.creation,
+                createdby: data.createdby
+            }
+        })
+        var data1 = datatoformat.filter(data=>{
+            if(data.createdby === 1){return true}
+            return false
+        })
+        var data2 = datatoformat.filter(data=>{
+            if(data.createdby !== 1){return true}
+            return false
+        })
+        setData(data1)
+        setDataallproposition(data2)
+    }
     const handleSubmit = async (data) => {
         console.log(data)
         setLoading(true);
         try {
             const dataproposition = await postPropositionPoste(data)
             const reponsemetie = await dataproposition;
-            console.log(reponsemetie)
-            setDataallproposition(reponsemetie)
+            mapingdata(reponsemetie)
             setLoading(false);
         }
         catch (error) {
@@ -70,11 +96,10 @@ function PropositionPageScreen() {
                 const reponsedatacompetanceaapi = await datacompetanceaapi;
                 const dataproposition = await listproposition()
                 const reponseproposition = await dataproposition;
-                setDataallproposition(reponseproposition)
                 setDataMetiercode(reponsemetie);
                 setDataCompetance(reponsecompetance);
                 setDataPropositionexistant(reponsedatacompetanceaapi)
-                setData([])
+                mapingdata(reponseproposition)
                 setLoading(false);
             } catch (error) {
               console.error('Une erreur s\'est produite :', error);
@@ -83,41 +108,114 @@ function PropositionPageScreen() {
             }
         };
         fetchData();
-    }, [setDataMetiercode, setDataCompetance, setDataPropositionexistant, setLoading, setError]);
+    }, [setDataMetiercode, setDataCompetance, setDataPropositionexistant, setLoading, setError, mapingdata]);
   
     const columns = useMemo(
         () => [
           {
-            accessorKey: 'code',
-            header: 'Code Rome',
-            size: 140,
+            accessorKey: 'id',
+            header: '',
+            size: 100,
+            enableEditing: false,
+            Header: ({ column }) => (
+                <EditIcon></EditIcon>
+            ),
+            Cell: ({ cell }) => 
+                (<Link>modifier</Link>)
+          },
+          {
+            accessorKey: 'metier',
+            header: 'Metiers',
+            size: 400,
             enableEditing: false,
           },
           {
-            accessorKey: 'nom',
-            header: 'Nom',
-            size: 140,
+            accessorKey: 'like',
+            header: 'Nombre de personne qui a un vote pour votre faveur',
+            size: 100,
             enableEditing: false,
+            Header: ({ column }) => (
+                <ThumbUpOffAltIcon></ThumbUpOffAltIcon>
+            ),
           },
           {
-            accessorKey: 'descriptionC',
-            header: 'Decription courte',
-            size: 140,
-            enableEditing: false,
-          },
-          {
-            accessorKey: 'descriptionL',
-            header: 'Decription longue',
-            size: 140,
+            accessorKey: 'unlike',
+            header: 'Nombre de personne qui a un vote contre votre faveur',
+            size: 100,
             enableHiding: true,
             enableEditing: false,
+            Header: ({ column }) => (
+                <ThumbDownOffAltIcon></ThumbDownOffAltIcon>
+            ),
           },
           {
             accessorKey: 'creation',
             header: 'Date création',
             enableColumnOrdering: true,
             enableEditing: false,
-            enableSorting: true,
+            Header: ({ column }) => (
+                <CalendarMonthIcon></CalendarMonthIcon>
+            ),
+          }
+        ],
+        [],
+    );
+    const columns2 = useMemo(
+        () => [
+          {
+            accessorKey: 'id',
+            header: '',
+            size: 100,
+            enableEditing: false,
+            Header: ({ column }) => (
+                <EditIcon></EditIcon>
+            ),
+            Cell: ({ cell }) => 
+                (<Link>Détails</Link>)
+          },
+          {
+            accessorKey: 'metier',
+            header: 'Metiers',
+            size: 400,
+            enableEditing: false,
+          },
+          {
+            accessorKey: 'like',
+            header: 'Nombre de personne qui a un vote pour votre faveur',
+            size: 100,
+            enableEditing: false,
+            Header: ({ column }) => (
+                <ThumbUpOffAltIcon></ThumbUpOffAltIcon>
+            ),
+          },
+          {
+            accessorKey: 'unlike',
+            header: 'Nombre de personne qui a un vote contre votre faveur',
+            size: 100,
+            enableHiding: true,
+            enableEditing: false,
+            Header: ({ column }) => (
+                <ThumbDownOffAltIcon></ThumbDownOffAltIcon>
+            ),
+          },
+          {
+            accessorKey: 'id',
+            header: 'Remettre mon vote à zero',
+            size: 100,
+            enableHiding: true,
+            enableEditing: false,
+            Header: ({ column }) => (
+                <BackspaceIcon></BackspaceIcon>
+            ),
+          },
+          {
+            accessorKey: 'creation',
+            header: 'Date création',
+            enableColumnOrdering: true,
+            enableEditing: false,
+            Header: ({ column }) => (
+                <CalendarMonthIcon></CalendarMonthIcon>
+            ),
           }
         ],
         [],
@@ -157,8 +255,8 @@ function PropositionPageScreen() {
                         },
                     }}
                 >
-                    {/* <Typography>Mes propositions</Typography>
-                    {TableMetier(columns,data)} */}
+                    <Typography>Mes propositions</Typography>
+                    {TableMetier(columns,data)}
                 </Grid>
                 <Grid item xs={12} md={2} my={5}>
                     <Button 
@@ -180,6 +278,52 @@ function PropositionPageScreen() {
                         size='large'
                         onClick={hangleopencreatemodal}>
                             crée un metier
+                    </Button>
+
+                    <Button 
+                        key="propositionfaite"
+                        variant="contained"
+                        color='primary'
+                        fullWidth
+                        style={buttonStyle}
+                        size='large'>
+                            Nb de propositions effectuées :
+                    </Button>
+                    <Button 
+                        key="pourcentproposition"
+                        variant="contained"
+                        color='primary'
+                        fullWidth
+                        style={buttonStyle}
+                        size='large'>
+                            % d’acceptations : 
+                    </Button>
+                </Grid>
+            </Grid>
+            <Grid container spacing={2} pl={5} pr={5}>
+                <Grid item xs={12} md={10}
+                    sx={{
+                        [theme.breakpoints.up('lg')]: {
+                            mt: 2,
+                        },
+                        [theme.breakpoints.down('sm')]: {
+                            my: 1,
+                            mx: 0,
+                        },
+                    }}
+                >
+                    <Typography>Propositions du réseau en cours</Typography>
+                    {TableMetier(columns2,allproposition)}
+                </Grid>
+                <Grid item xs={12} md={2} my={5}>
+                    <Button 
+                        key="filtre"
+                        variant="contained"
+                        color='primary'
+                        fullWidth
+                        style={buttonStyle}
+                        size='large'>
+                            Filtrer sur non répondu 
                     </Button>
                 </Grid>
             </Grid>
