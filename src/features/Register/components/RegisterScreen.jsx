a5e86cd6e52cdce1bf71886f837808dfdfe33759
Félin Aldric <fel1aldric@gmail.com>
2023-09-15 14:50:06 +0300

    [feat] form validation inscription

--- src/features/Register/components/RegisterScreen.jsx
+++ src/features/Register/components/RegisterScreen.jsx
@@ -27,11 +27,48 @@ export default function RegisterScreen() {
         'password':"",
         "password2":""
     });
+    const [ErrorForm, setErrorForm] = useState({
+        "siret": false,
+        "naf": false,
+        "nom_entreprise": false,
+        'rue_numero': false,
+        'code_postal': false,
+        'ville': false,
+        'pays':false,
+        'telephone':false,
+        'email':false,
+        'effectif':false,
+        'etablissement':false,
+        'nomrh': false,
+        'prenomrh': false,
+        'fonctionrh': false,
+        'servicerh': false,
+        'telephonerh':false,
+        'emailrh':false,
+        'password':false,
+        "password2":false
+    });
 
     return (
         <>
-            {screen === 1 && <ScreenOne setScreen={setScreen} setFormData={setFormData} formData={formData} />}
-            {screen === 2 && <ScreenTwo formData={formData} setScreen={setScreen} setFormData={setFormData}/>}
+            {screen === 1 && 
+                <ScreenOne 
+                    setScreen={setScreen}
+                    setFormData={setFormData}
+                    formData={formData}
+                    ErrorForm={ErrorForm}
+                    setErrorForm={setErrorForm}
+                />
+            }
+            {screen === 2 && 
+                <ScreenTwo
+                    formData={formData}
+                    setScreen={setScreen}
+                    setFormData={setFormData}
+                    ErrorForm={ErrorForm}
+                    setErrorForm={setErrorForm}
+                />
+            }
         </>
     )
 }
